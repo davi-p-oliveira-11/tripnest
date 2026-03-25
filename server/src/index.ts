fbf31c2a86573dbcb7519cfb081cbbf60923e5ee
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-03-25 12:54:06 -0300

    feat(server): connect to MongoDB using mongoose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,19 @@
-import express, { Request, Response} from 'express'
-import cors from 'cors'
-import "dotenv/config"
+import express, { Request, Response} from 'express';
+import cors from 'cors';
+import "dotenv/config";
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING as string);
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors())
+app.use(cors());
 
 app.get("/api/test", async (req: Request, res: Response) => {
   res.json({ message: "hallo mars !"});
 })
 
 app.listen(7000, () => {
-   console.log("server running on port 7000")   
+   console.log("server running on port 7000"); 
 })
