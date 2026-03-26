1f062453ccbc3d9e321e1f77139d6e0f90735b76
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-03-25 22:24:04 -0300

    feat(server): add login route with validation, bcrypt password check and JWT cookie auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
-import express, { Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
 import userRoutes  from './routes/users';
+import authRoutes from './routes/auth'
 
 mongoose.connect(process.env.MONGO_CONNECTION_STRING as string);
 
@@ -11,7 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
-app.use("/api/users", userRoutes) 
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes); 
 
 
 app.listen(7000, () => {
