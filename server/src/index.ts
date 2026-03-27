17a1def2070ee42f7ea3ec27cf003d36c685d380
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-03-27 01:16:03 -0300

    feat(e2e): implement Playwright tests for user sign in and registration flows

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,27 +2,34 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
-import userRoutes  from './routes/users';
+import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
 import cookieParser from "cookie-parser";
 
-mongoose.connect(process.env.MONGO_CONNECTION_STRING as string);
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+   {/*
+   .then(() =>
+       console.log("Connected to database: ", 
+       process.env.MONGODB_CONNECTION_STRING
+       )
+    );
+    */}
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(
    cors({
-     origin: process.env.FRONTEND_URL,
-     credentials: true,
+      origin: process.env.FRONTEND_URL,
+      credentials: true,
    })
 );
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes); 
+app.use("/api/users", userRoutes);
 
 
 app.listen(7000, () => {
-   console.log("server running on port 7000"); 
+   console.log("server running on port 7000");
 })
