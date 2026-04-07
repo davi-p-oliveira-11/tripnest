dc33ea0a62aaa48ea876ab96243e9e52690f800e
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-04-06 21:40:57 -0300

    feat(booking): implement booking flow with Stripe payments, API, frontend UI, and e2e tests

--- e2e/tests/search-hotels.spec.ts
+++ e2e/tests/search-hotels.spec.ts
@@ -18,6 +18,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 
+
 test("should show hotel search results", async ({ page }) => {
   await page.goto(UI_URL);
 
@@ -38,4 +39,36 @@ test("should show hotel detail", async ({ page }) => {
   await page.getByText("Halala Hotel").click();
   await expect(page).toHaveURL(/detail/);
   await expect(page.getByRole("button", { name: "Book now" })).toBeVisible();
+});
+
+test("should book hotel", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("Where are you going?").fill("New York");
+
+  const date = new Date();
+  date.setDate(date.getDate() + 3);
+  const formattedDate = date.toISOString().split("T")[0];
+  await page.getByPlaceholder("Check-out Date").fill(formattedDate);
+
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await page.getByText("Dublin Getaways").click();
+  await page.getByRole("button", { name: "Book now" }).click();
+
+  await expect(page.getByText("Total Cost: £357.00")).toBeVisible();
+
+  const stripeFrame = page.frameLocator("iframe").first();
+  await stripeFrame
+    .locator('[placeholder="Card number"]')
+    .fill("4242424242424242");
+  await stripeFrame.locator('[placeholder="MM / YY"]').fill("04/30");
+  await stripeFrame.locator('[placeholder="CVC"]').fill("242");
+  await stripeFrame.locator('[placeholder="ZIP"]').fill("24225");
+
+  await page.getByRole("button", { name: "Confirm Booking" }).click();
+  await expect(page.getByText("Booking Saved!")).toBeVisible();
+
+  await page.getByRole("link", { name: "My Bookings" }).click();
+  await expect(page.getByText("Dublin Getaways")).toBeVisible();
 });
